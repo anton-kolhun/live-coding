6487f092aa0262216b5659e92a22ea1bf75d2835
anton <mag-ua@ukr.net>
2020-06-10 14:48:22 +0300

    add tasks

--- staircase_options.py
+++ staircase_options.py
@@ -2,7 +2,6 @@
 Davis has a number of staircases in his house and he likes to climb each staircase 1, 2, or 3 steps at a time.
 He wonders how many ways there are to reach the top of the staircase.
 
-
 Given the respective heights for each of the  staircases in his house, find and print the number of ways
 he can climb staircase
 
