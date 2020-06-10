5a1db952e3314444d53d5e1ffb3b318e21f749c2
anton <mag-ua@ukr.net>
2020-06-10 14:45:44 +0300

    add tasks

---
+++ make_anagrams.py
@@ -0,0 +1,20 @@
+"""
+We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form
+the second string. In other words, both strings must contain the same exact letters in the same exact frequency
+
+Given two strings, a and b that may or may not be of the same length, determine the minimum number of character
+deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+
+For example, if a = 'cde' and b = 'dcf' we can delete 'e' from string A and 'f' from string B
+so that both remaining strings are  and  which are anagrams.
+"""
+
+
+def makeAnagram(a, b):
+    # TODO: your implementation goes her
+    return 0
+
+
+if __name__ == '__main__':
+    result = makeAnagram('etst', 'test2')
+    assert result == 1, '"etst" is converted to "test" with 0 deletions. "test2" is converted to "test" with 1 deletion'
---
+++ staircase_options.py
@@ -0,0 +1,36 @@
+"""
+Davis has a number of staircases in his house and he likes to climb each staircase 1, 2, or 3 steps at a time.
+He wonders how many ways there are to reach the top of the staircase.
+
+
+Given the respective heights for each of the  staircases in his house, find and print the number of ways
+he can climb staircase
+
+For example, there is a staircase in the house that is 5 steps high. Davis can step on the following sequences of steps:
+
+1 1 1 1 1
+1 1 1 2
+1 1 2 1
+1 2 1 1
+2 1 1 1
+1 2 2
+2 2 1
+2 1 2
+1 1 3
+1 3 1
+3 1 1
+2 3
+3 2
+
+Therefore, 13 ways in total to reach the top
+"""
+
+
+def stepOptions(n):
+    # TODO: your implementation goes her
+    return -1
+
+
+if __name__ == '__main__':
+    result = stepOptions(3)
+    assert result == 4, 'length of [111, 12, 21, 3] equals 4'
